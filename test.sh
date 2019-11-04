f0df20c6697cf8d57847291ca02f12edbb5cb668
Adrien Ferrand <ferrand.ad@gmail.com>
2019-11-04 16:56:33 +0100

    Install wheel

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 set -ex
 
 python3 -m venv venv
+venv/bin/python -m pip install wheel
 venv/bin/python -m pip install -e certbot/certbot-ci
 venv/bin/python -m pytest certbot/certbot-ci/certbot_integration_tests -n 4
 
